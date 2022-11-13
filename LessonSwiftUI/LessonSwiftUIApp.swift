ba4b355cf539abb915b60acae8c6292e0cf8df8d
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2022-11-13 13:55:28 +0300

    Initial Commit

---
+++ LessonSwiftUI/LessonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonSwiftUIApp.swift
+//  LessonSwiftUI
+//
+//  Created by Ilya Vasilev on 13.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct LessonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
